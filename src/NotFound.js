117e865286113525d57ff554598c0a48f002e806
Vera Chashchina <chaschinavera@gmail.com>
2024-10-15 15:22:33 +0300

    create page NotFound

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center w-[100%] min-h-[100vh] bg-black text-white">
+      <h1 className="text-[30px] md:text-[40px] mt-[15px] mx-[30px] text-center">
+        Страница не найдена
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/Person.js
+++ src/Person.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import NotFound from "./NotFound";
 
 const Person = () => {
   const id = window.location.pathname.split("/").pop();
@@ -15,44 +16,53 @@ const Person = () => {
   }, []);
 
   const person = persons.find((item) => item.id == id);
-
-  console.log(person);
+  const regEx = /^([1-9]|[1-4][\d]|5[0-1])$/;
 
   return (
-    <div className="bg-black text-white w-[100%] min-h-[100vh] flex flex-col items-center py-[50px]">
-      <h1 className="text-[30px] md:text-[40px] mt-[15px] text-center">Вселенная Рика и Морти</h1>
-      <div className="flex flex-col items-start mt-[50px]">
-        <p className="text-[20px] md:text-[30px] mt-[15px]">Имя: {person?.name}</p>
-        <p className="text-[20px] md:text-[30px] mt-[15px]">
-          Жив: {person?.status === "Dead" ? "Нет" : "Да"}
-        </p>
-        <p className="text-[20px] md:text-[30px] mt-[15px]">
-          Раса:
-          {person?.species === "Human"
-            ? "Человек"
-            : person?.species === "Alien"
-            ? "Инопланетянин"
-            : "Неизвестно"}
-        </p>
-        <p className="text-[20px] md:text-[30px] mt-[15px]">
-          Пол:
-          {person?.gender === "Male"
-            ? "Мужской"
-            : person?.gender === "Female"
-            ? "Женский"
-            : "Неизвестно"}
-        </p>
-        <p className="text-[20px] md:text-[30px] mt-[15px]">
-          Количество эпизодов: {person?.episode.length}
-        </p>
-      </div>
-      <a
-        href="/rick-and-morty/"
-        className="text-[20px] md:text-[30px] mt-[100px] item-start hover:text-red-500 cursor-pointer"
-      >
-        Назад
-      </a>
-    </div>
+    <>
+      {!regEx.test(person?.id) ? (
+        <NotFound />
+      ) : (
+        <div className="bg-black text-white w-[100%] min-h-[100vh] flex flex-col items-center py-[50px]">
+          <h1 className="text-[30px] md:text-[40px] mt-[15px] mx-[30px] text-center">
+            Вселенная Рика и Морти
+          </h1>
+          <div className="flex flex-col items-start mt-[50px]">
+            <p className="text-[20px] md:text-[30px] mt-[15px]">
+              Персонаж {person?.name}
+            </p>
+            <p className="text-[20px] md:text-[30px] mt-[15px]">
+              Жив: {person?.status === "Dead" ? "Нет" : "Да"}
+            </p>
+            <p className="text-[20px] md:text-[30px] mt-[15px]">
+              Раса:{" "}
+              {person?.species === "Human"
+                ? "Человек"
+                : person?.species === "Alien"
+                ? "Инопланетянин"
+                : "Неизвестно"}
+            </p>
+            <p className="text-[20px] md:text-[30px] mt-[15px]">
+              Пол:{" "}
+              {person?.gender === "Male"
+                ? "Мужской"
+                : person?.gender === "Female"
+                ? "Женский"
+                : "Неизвестно"}
+            </p>
+            <p className="text-[20px] md:text-[30px] mt-[15px]">
+              Количество эпизодов: {person?.episode.length}
+            </p>
+          </div>
+          <a
+            href="/rick-and-morty/"
+            className="text-[20px] md:text-[30px] mt-[100px] item-start hover:text-red-500 cursor-pointer"
+          >
+            Назад
+          </a>
+        </div>
+      )}
+    </>
   );
 };
 
