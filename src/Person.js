40538230623b3dff9c356ee295a7451a0ec0bf4f
Vera Chashchina <chaschinavera@gmail.com>
2024-10-15 13:32:17 +0300

    fix person component

--- src/Person.js
+++ src/Person.js
@@ -15,31 +15,33 @@ const Person = () => {
   }, []);
 
   const person = persons.find((item) => item.id == id);
+  
+  console.log(person);
 
   return (
     <div className="bg-black text-white w-[100%] min-h-[100vh] flex flex-col items-center py-[50px]">
-      <h1 className="text-[40px] mt-[15px]">Имя: {person.name}</h1>
+      <h1 className="text-[40px] mt-[15px]">Имя: {person?.name}</h1>
       <p className="text-[30px] mt-[15px]">
-        Жив: {person.status === "Dead" ? "Нет" : "Да"}
+        Жив: {person?.status === "Dead" ? "Нет" : "Да"}
       </p>
       <p className="text-[30px] mt-[15px]">
-        Раса:{" "}
-        {person.species === "Human"
+        Раса:
+        {person?.species === "Human"
           ? "Человек"
-          : person.species === "Alien"
+          : person?.species === "Alien"
           ? "Инопланетянин"
           : "Неизвестно"}
       </p>
       <p className="text-[30px] mt-[15px]">
-        Пол:{" "}
-        {person.gender === "Male"
+        Пол:
+        {person?.gender === "Male"
           ? "Мужской"
-          : person.gender === "Female"
+          : person?.gender === "Female"
           ? "Женский"
           : "Неизвестно"}
       </p>
       <p className="text-[30px] mt-[15px]">
-        Количество эпизодов: {person.episode.length}
+        Количество эпизодов: {person?.episode.length}
       </p>
       <a
         href="/"
