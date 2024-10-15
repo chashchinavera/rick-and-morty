426fc06ffd93bc34b8b486aa0d6ceaca1fcb8b12
Vera Chashchina <chaschinavera@gmail.com>
2024-10-15 15:32:05 +0300

    create NotFound page route

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import App from "./App";
 import Person from "./Person";
+import NotFound from "./NotFound";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="rick-and-morty/" element={<App />} />
       <Route path="rick-and-morty/:id" element={<Person />} />
+      <Route path="rick-and-morty/*" element={<NotFound />} />
     </Routes>
   );
 };
